9e7d4c23918b1dcb2b15ed3025a5e9c5c06c8165
Sandun <sandundimaria7@gmail.com>
2023-12-27 12:29:55 +0530

    added the map component

--- frontend/src/Forms/Forms.js
+++ frontend/src/Forms/Forms.js
@@ -1,78 +1,76 @@
 import React, { useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Papa from "papaparse";
 import CsvTable from "../Dashboard/CsvTable"; // Make sure to use the correct path
+import "./form.css";
 
 const Forms = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [csvData, setCsvData] = useState(null);
-  const [uploadMessage, setUploadMessage] = useState(null);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
-    // Reset messages when a new file is selected
-    setUploadMessage(null);
   };
 
   const handleUpload = async () => {
     if (!selectedFile) {
-      setUploadMessage("No file selected");
+      console.log("No file selected");
       return;
     }
 
-    setUploadMessage("Please Wait, Making Predictions...");
-
     const formData = new FormData();
-    formData.append("file", selectedFile);
+    formData.append('file', selectedFile);
 
     try {
-      const response = await fetch("http://localhost:5000/upload", {
-        method: "POST",
+      const response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
         body: formData,
       });
 
-      if (response.ok) {
-        setUploadMessage("Predictions Made, view Table Below");
+      const result = await response.text();
 
-        const result = await response.text();
+      // Parse the CSV data using papaparse
+      Papa.parse(result, {
+        header: true,
+        complete: function (parsedResult) {
+          console.log("Parsed CSV Data:", parsedResult.data);
 
-        // Parse the CSV data using papaparse
-        Papa.parse(result, {
-          header: true,
-          complete: function (parsedResult) {
-            console.log("Parsed CSV Data:", parsedResult.data);
-
-            // Set the parsed CSV data to state
-            setCsvData(parsedResult.data);
-          },
-          error: function (error) {
-            console.error("Error parsing CSV:", error.message);
-            setUploadMessage("Error parsing CSV: " + error.message);
-          },
-        });
-      } else {
-        setUploadMessage("Error uploading file. Please try again.");
-      }
+          // Set the parsed CSV data to state
+          setCsvData(parsedResult.data);
+        },
+        error: function (error) {
+          console.error('Error parsing CSV:', error.message);
+        },
+      });
     } catch (error) {
-      console.error("Error uploading file:", error);
-      setUploadMessage("Error uploading file: " + error.message);
+      console.error('Error uploading file:', error);
     }
   };
 
   return (
     <div className="form1">
-      <Form>
-        <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>Choose a file</Form.Label>
-          <Form.Control type="file" onChange={handleFileChange} />
-        </Form.Group>
-        <Button variant="primary" onClick={handleUpload}>
-          Upload
-        </Button>
-      </Form>
-
-      {/* Display upload message */}
-      {uploadMessage && <Alert variant="info">{uploadMessage}</Alert>}
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col xs={6}>
+            <div className="sub-container">
+              <Form>
+                <Form.Group controlId="formFile" className="mb-3">
+                  <Form.Label>Choose a file</Form.Label>
+                  <Form.Control type="file" onChange={handleFileChange} />
+                </Form.Group>
+                <Button variant="primary" onClick={handleUpload}>
+                  Upload
+                </Button>
+              </Form>
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
 
       {/* Display the CSV data in a table */}
       {csvData && <CsvTable csvData={csvData} />}
--- frontend/src/Map/Maps.js
+++ frontend/src/Map/Maps.js
@@ -1,62 +1,100 @@
-// src/components/DistanceCalculator.js
+import React, { useEffect, useState } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import "./maps.css";
 
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import axios from 'axios';
+const mapStyles = {
+  width: "90%",
+  height: "700px",
+  margin: "auto",
+};
 
-const DistanceCalculator = () => {
-  const [locationA, setLocationA] = useState('');
-  const [locationB, setLocationB] = useState('');
-  const [distance, setDistance] = useState(null);
+const locations = [
+  { lat: 6.964029, lng: 79.897132, label: "A" },
+  { lat: 6.986521, lng: 79.936442, label: "B" },
+  { lat: 7.051093, lng: 79.866576, label: "C" },
+  { lat: 7.113161, lng: 79.883399, label: "D" },
+  { lat: 7.178056, lng: 79.892326, label: "E" },
+  { lat: 7.163409, lng: 79.983821, label: "F" },
+  { lat: 6.950222, lng: 79.976905, label: "G" },
+  { lat: 6.852967, lng: 80.084421, label: "H" },
+  { lat: 6.795612, lng: 80.142464, label: "I" },
+  { lat: 6.612645, lng: 80.220264, label: "J" },
+];
 
-  const calculateDistance = async () => {
-    try {
-      const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${locationA}&destinations=${locationB}&key=${process.env.key}`
-      );
+const MapContainer = (props) => {
+  const [legDistances, setLegDistances] = useState([]);
 
-      const result = response.data.rows[0].elements[0];
-      setDistance(result.distance.text);
-    } catch (error) {
-      console.error('Error calculating distance:', error);
-    }
-  };
+  useEffect(() => {
+    const map = new props.google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: { lat: 6.92696, lng: 79.86138 },
+    });
+
+    const directionsService = new props.google.maps.DirectionsService();
+    const directionsRenderer = new props.google.maps.DirectionsRenderer();
+
+    directionsRenderer.setMap(map);
+
+    const waypoints = locations.map((location) => ({
+      location: new props.google.maps.LatLng(location.lat, location.lng),
+      stopover: true,
+    }));
+
+    const request = {
+      origin: waypoints[0].location,
+      destination: waypoints[waypoints.length - 1].location,
+      waypoints: waypoints.slice(1, waypoints.length - 1),
+      travelMode: props.google.maps.TravelMode.DRIVING,
+    };
+
+    directionsService.route(request, (result, status) => {
+      if (status === props.google.maps.DirectionsStatus.OK) {
+        directionsRenderer.setDirections(result);
+
+        // Calculate and set leg distances
+        const distances = result.routes[0].legs.map((leg) => {
+          return {
+            distance: leg.distance.text,
+            startLocation: leg.start_address,
+            endLocation: leg.end_address,
+          };
+        });
+
+        setLegDistances(distances);
+      } else {
+        console.error("Error fetching directions:", status);
+      }
+    });
+
+    locations.forEach((location) => {
+      new props.google.maps.Marker({
+        position: { lat: location.lat, lng: location.lng },
+        map: map,
+        label: location.label,
+      });
+    });
+  }, [props.google.maps.Map]);
 
   return (
     <div>
-      <Form>
-        <Form.Group controlId="locationA">
-          <Form.Label>Location A</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter location A"
-            value={locationA}
-            onChange={(e) => setLocationA(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="locationB">
-          <Form.Label>Location B</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter location B"
-            value={locationB}
-            onChange={(e) => setLocationB(e.target.value)}
-          />
-        </Form.Group>
-
-        <Button variant="primary" onClick={calculateDistance}>
-          Calculate Distance
-        </Button>
-      </Form>
-
-      {distance && (
-        <div className="mt-3">
-          <p>Distance: {distance}</p>
-        </div>
-      )}
+      <div id="map" style={mapStyles}></div>
+      <div className="leg-distances">
+        <p>Leg Distances:</p>
+        <ul>
+          {legDistances.map((leg, index) => (
+            <li key={index}>
+              <span className="start-location">{leg.startLocation}</span>
+              <span className="separator"> to </span>
+              <span className="end-location">{leg.endLocation}</span>
+              <span className="distance">: {leg.distance}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default DistanceCalculator;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyD1-kK-XpZFXmP9H3IoCRMr2qtblWLp1tE",
+})(MapContainer);
